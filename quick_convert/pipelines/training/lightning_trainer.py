c748c49c729a0cb830195dc4fd810e9aef80037e
Ben Luks <luksben@gmail.com>
2026-05-26 15:13:23 +0100

    move all training modules under trainer pipeline (/system) folder)

---
+++ quick_convert/pipelines/training/lightning_trainer.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+import lightning as L
+
+from quick_convert.data.base_dataset import BaseDataset
+from .base_trainer import BaseTrainer
+
+
+class LightningTrainer(BaseTrainer):
+    def __init__(
+        self,
+        module,
+        train_dataloader_kwargs: Optional[dict] = {},
+        val_dataloader_kwargs: Optional[dict] = {},
+    ):
+
+        self.module = module
+        self.train_dataloader_kwargs = train_dataloader_kwargs
+        self.val_dataloader_kwargs = val_dataloader_kwargs
+
+    def train(
+        self,
+        train_dataset: BaseDataset,
+        val_dataset: Optional[BaseDataset] = None,
+        output_dir=None,
+        kwargs: Optional[dict] = {},
+    ):
+
+        self.pl_trainer = L.Trainer(default_root_dir=output_dir, **kwargs)
+
+        train_loader = train_dataset.make_dataloader(**self.train_dataloader_kwargs)
+        val_loader = val_dataset.make_dataloader(**self.val_dataloader_kwargs) if val_dataset else None
+
+        return self.pl_trainer.fit(
+            model=self.module,
+            train_dataloaders=train_loader,
+            val_dataloaders=val_loader,
+        )
---
+++ quick_convert/pipelines/training/pipeline.py
@@ -0,0 +1,35 @@
+# quick_convert/pipelines/training/train.py
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Any, Optional
+
+from quick_convert.pipelines.training.base_trainer import BaseTrainer
+
+
+class TrainingPipeline:
+    def __init__(
+        self,
+        trainer: BaseTrainer,
+        train_dataset: Any | None = None,
+        val_dataset: Any | None = None,
+        test_dataset: Any | None = None,
+        output_dir: str | Path | None = None,
+        train_kwargs: Optional[dict] = None,
+    ) -> None:
+        self.trainer = trainer
+        self.train_dataset = train_dataset
+        self.val_dataset = val_dataset
+        self.test_dataset = test_dataset
+        self.output_dir = output_dir
+        self.train_kwargs = train_kwargs
+
+    def run(self) -> Any:
+        return self.trainer.train(
+            train_dataset=self.train_dataset,
+            val_dataset=self.val_dataset,
+            test_dataset=self.test_dataset,
+            output_dir=self.output_dir,
+            kwargs=self.train_kwargs,
+        )
