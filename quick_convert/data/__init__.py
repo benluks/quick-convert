812941dd242207c884dc3e75c30492feaf51d38c
Ben Luks <luksben@gmail.com>
2026-05-29 11:58:43 +0100

    fix manifest dataset support

--- quick_convert/data/__init__.py
+++ quick_convert/data/__init__.py
@@ -2,5 +2,6 @@ from __future__ import annotations
 
 from .types import AudioBatch, AudioSample
 from .base_dataset import BaseDataset
+from .manifest_dataset import ManifestDataset
 
-__all__ = ["AudioBatch", "AudioSample", "BaseDataset"]
+__all__ = ["AudioBatch", "AudioSample", "BaseDataset", "ManifestDataset"]
