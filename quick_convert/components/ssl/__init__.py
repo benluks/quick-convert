8c3fd8796a488f31cfe378af176e46397f600948
Francisco Saraiva Sepúlveda Teixeira <francisco.sep.teixeira@l2f.inesc-id.pt>
2026-06-08 13:40:27 +0100

    Add  W2VBertContentEncoderDS (downsampled w2v-bert 2)

--- quick_convert/components/ssl/__init__.py
+++ quick_convert/components/ssl/__init__.py
@@ -2,10 +2,11 @@
 
 from .emo2vec import EmotionEncoder
 from .pros2vec import ProsodyEncoder
-from .w2vbert import W2VBertContentEncoder
+from .w2vbert import W2VBertContentEncoder, W2VBertContentEncoderDS
 
 __all__ = [
     "EmotionEncoder",
     "ProsodyEncoder",
     "W2VBertContentEncoder",
+    "W2VBertContentEncoderDS",
 ]
